5343b21c73eae07cbf934cf811bf53a2fe0eb395
Arigol <antonio.rigolino@gmail.com>
2021-12-08 16:13:44 -0300

    Adicionei a posiçao de cada irmao no mapa para facilitar o movimento!

--- maps.cpp
+++ maps.cpp
@@ -2,8 +2,18 @@
 #include <bits/stdc++.h>
 
 struct gamemap{
-    int xDim,yDim;// x = numero de linhas | y = tamanho das linhas
-    std::vector<std::string> contents;//vetor de de linhas do mapa!
+    // x = numero de linhas | y = tamanho das linhas
+    int xDim,yDim;
+
+
+    //tupla que define (x,y) da posição de cada irmão no mapa
+    std::pair<int,int> marioPos; // como convenção vou adotar mario como M...
+    std::pair<int,int> luigiPos; // ... e Luigi como L
+
+    // vetor de zonas criticas que podem conter apenas 1 dos irmãos!
+    std::vector<int> criticalZones;
+    //vetor de de linhas do mapa!
+    std::vector<std::string> contents;
 };
 
 map_t *readMap(std::string mapName){
@@ -25,6 +35,13 @@ map_t *readMap(std::string mapName){
             newMap->yDim = linha.size();
             newMap->xDim ++;
             newMap->contents.push_back(linha);
+
+            //identificando a posição dos irmão na linha!
+            std::size_t posYM = linha.find('M');
+            std::size_t posYL = linha.find('L');
+            if(posYL != std::string::npos) newMap->luigiPos = std::make_pair(newMap->xDim-1,(int)posYL);
+            if(posYM != std::string::npos) newMap->marioPos = std::make_pair(newMap->xDim-1,(int)posYM);
+
         }
     }
 
@@ -38,4 +55,5 @@ void printMap(map_t *mapa){
     {
         std::cout << linha << std::endl;
     }
+    printf("Mario pos: ( %d, %d )\tLuigi pos: ( %d, %d )\n",mapa->marioPos.first,mapa->marioPos.second,mapa->luigiPos.first,mapa->luigiPos.second);
 }
